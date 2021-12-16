e7568e12dede187894033ff1a971fe26c1abd65c
A. M Manzano Kharman <40214590+aidamanzano@users.noreply.github.com>
2021-12-16 21:28:55 +0000

    found where the actual shapley values are!

--- test.py
+++ test.py
@@ -2,20 +2,29 @@
 import shap
 import matplotlib.pyplot as plt  # To visualize
 import pandas as pd  # To read data
-import xgboost
+import sklearn
 
 data = pd.read_csv("datapoints.csv")
 
+#X = data[['date', 'time', 'latitude', 'longitude']] I'm not sure what the best model would be to pick for all 4 attributes
 X = data[['latitude', 'longitude']]
 y = data[['probability']]
 
-model = xgboost.XGBRegressor().fit(X, y)
-print(model)
 
-# explain the model's predictions using SHAP
-# (same syntax works for LightGBM, CatBoost, scikit-learn, transformers, Spark, etc.)
-explainer = shap.Explainer(model)
+# a simple linear model
+model = sklearn.linear_model.LinearRegression()
+model.fit(X, y)
+
+#SHAP values
+explainer = shap.Explainer(model.predict, X)
 shap_values = explainer(X)
 
-# visualize the first prediction's explanation
-shap.plots.waterfall(shap_values[0])
+
+print('SHAPLEY VALUES', shap_values.values) #this returns the shapley value of each value of each datapoint.
+
+"""we can plot the SHAP values of every feature for every sample. The plot below sorts features by the sum of SHAP value magnitudes 
+over all samples, and uses SHAP values to show the distribution of the impacts each feature has on the model output. 
+The color represents the feature value (red high, blue low). 
+This reveals for example that a high latitude increases the predicted probability that the parking slot is empty."""
+
+shap.plots.beeswarm(shap_values)
